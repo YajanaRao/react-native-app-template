5d6afe1481250272918e2b3aa3a5a70a412a239e
YajanaRao <yajananrao@gmail.com>
2021-06-28 09:55:38 +0530

    first commit

--- App.tsx
+++ App.tsx
@@ -1,19 +1,75 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as eva from '@eva-design/eva';
+import { ApplicationProvider, Layout, Text, Button, Icon, IconRegistry } from '@ui-kitten/components';
+import { EvaIconsPack } from '@ui-kitten/eva-icons';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { View } from 'react-native';
+import store from './app/store'
+import { Provider, useDispatch, useSelector } from 'react-redux'
+import { decrement, increment } from './app/features/counter/counterSlice';
 
-export default function App() {
+const HomeScreen = ({ navigation }) => {
+  const count = useSelector((state) => state.counter.value)
+  const dispatch = useDispatch()
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
+    <Layout style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text category='h1'>HOME</Text>
+      <Text category='h2'>{count}</Text>
+      <View style={{ flexDirection: 'row' }}>
+        <Button
+          accessoryLeft={(props) => <Icon {...props} name="plus-circle-outline" />}
+          style={{ margin: 2 }}
+          onPress={() => dispatch(increment())}
+        >
+          Increment
+        </Button>
+        <Button
+          accessoryLeft={(props) => <Icon {...props} name="minus-circle-outline" />}
+          style={{ margin: 2 }}
+          appearance="outline"
+          onPress={() => dispatch(decrement())}
+        >
+          Decrement
+        </Button>
+      </View>
+      <Button onPress={() => navigation.navigate('Details')}
+      >
+        Details
+      </Button>
+    </Layout>
+  )
+}
+const DetailScreen = ({ navigation }) => (
+  <Layout style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+    <Text category='h1'>DETAILS</Text>
+    <Button onPress={() => navigation.navigate("Home")}
+    >
+      Home
+    </Button>
+  </Layout>
+);
+
+
+
+
+const Stack = createStackNavigator();
+
+function App() {
+  return (
+    <Provider store={store}>
+      <IconRegistry icons={EvaIconsPack} />
+      <ApplicationProvider {...eva} theme={eva.light}>
+        <NavigationContainer>
+          <Stack.Navigator>
+            <Stack.Screen name="Home" component={HomeScreen} />
+            <Stack.Screen name="Details" component={DetailScreen} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </ApplicationProvider>
+    </Provider>
+
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
